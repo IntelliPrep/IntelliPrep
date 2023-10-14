89bae16aa62cec3fefb25d772fb22ffa668d33c0
Krish Katariya <krish.katariya@hotmail.com>
2023-10-14 12:44:06 -0400

    Added functionality for posting to flask server for data

--- client/src/components/InputData.js
+++ client/src/components/InputData.js
@@ -2,15 +2,25 @@ import React from 'react'
 import "./InputData.css"
 import axios from "axios"
 import { useState } from 'react'
+import {  } from "react-async"
 
 const InputData = () => {
+    const client = axios.create({ baseURL: "http://127.0.0.1:5000"});
+
     const [profileData, setProfileData] = useState(null)
+    const [sendData, setSendData] = useState(null);
+    async function getProfileData() {
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/algorithmSend', {data: sendData});
+            setProfileData(({
+                profile_name : response.data.name,
+                about_me: response.data.about}));
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
     function getData() {
-        axios({
-          method: "GET",  
-          url:"http://127.0.0.1:5000/algorithm",
-        })
-        .then((response) => {
+        client.get("/algorithm").then((response) => {
           const res = response.data
           setProfileData(({
             profile_name: res.name,
@@ -31,8 +41,8 @@ const InputData = () => {
         
         <div className='name'>
             <label for="testname">Test Name:</label>
-            <input type="text" name="testname" />
-            <button onClick={getData}>Click me</button>
+            <input type="text" value={sendData} onChange={(e) => setSendData(e.target.value)} name="testname"/>
+            <button onClick={getProfileData}>Click me</button>
         </div>
         {profileData && <div>
               <p>Profile name: {profileData.profile_name}</p>
